0ff42b0ea2fcad3f3211c3b57f3791b103365c4f
Nguyễn Thái Hải <85157423+ThaiHaiDev@users.noreply.github.com>
2023-02-14 14:22:30 +0700

    setup project

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/css/grid.scss';
+import './assets/css/index.scss';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
